687324a9af4e9f7fdca4e121a1c020b57e8198e7
Kush <kooshpatel@gmail.com>
2022-06-24 14:22:19 -0400

    Delete [vehicles] directory

--- [vehicles]/fifthgatecars/vehicle_names.lua
+++
@@ -1,3 +0,0 @@
-CreateThread(function()
-    AddTextEntry('gamename', 'Display Name') -- Gamename = Vehicle model name // Display Name = Name it shows ingame
-end)
--- [vehicles]/realistichandling/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'adamant'
-
-game 'gta5'
-
-data_file 'HANDLING_FILE' 'handling.meta'
-
-files {
-	'handling.meta'
-}
